be65403633dd85df4d5b3096acdc0ec8dbc0b31f
Modamori Oluwayomi <77457879+Portia-dot@users.noreply.github.com>
2023-11-17 18:40:35 -0800

    Initial Commit

---
+++ AirBnbClone/AirBnbCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirBnbCloneApp.swift
+//  AirBnbClone
+//
+//  Created by Oluwayomi M on 2023-11-17.
+//
+
+import SwiftUI
+
+@main
+struct AirBnbCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
